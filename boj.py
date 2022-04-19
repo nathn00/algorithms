5372adb568f7dbcb53476852d3d5c89cffb729f5
CWCTBOY <qkrdlswo98@gmail.com>
2022-04-20 02:16:51 +0900

    boj

--- boj.py
+++ boj.py
@@ -576,3 +576,50 @@
 #     if arr[i] < 40:
 #         arr[i] = 40
 # print(int(sum(arr)/5))
+
+# 1934
+# T = int(input())
+# for i in range(T):
+#     A, B = map(int, input().split())
+#     A_origin = A
+#     B_origin = B
+#     a = 1
+#     b = 1
+#     arr_a = []
+#     arr_b = []
+#     arr_ab = []
+#     while A - a != 0:
+#         a += 1
+#         if A % a == 0:
+#             arr_a.append(a)
+#     while B - b != 0:
+#         b += 1
+#         if B % b == 0:
+#             arr_b.append(b)
+#     for s in range(len(arr_a)):
+#         if arr_a[s] in arr_b:
+#             arr_ab.append(arr_a[s])
+#     if len(arr_ab) == 0:
+#         print(A_origin * B_origin)
+#     else:
+#         print(int((A_origin*B_origin)/arr_ab[-1]))
+
+# 9506
+while True:
+    n = int(input())
+    n_origin = n
+    arr = ['1']
+    i = 1
+    if n != -1:
+        while n - i != 0:
+            i += 1
+            if n % i == 0:
+                arr.append(str(i))
+        arr_int = map(int, arr)
+        integrate = sum(arr_int) - n_origin
+        if integrate != n_origin:
+            print(f'{n_origin} is NOT perfect.')
+        else:
+            print(f'{n_origin} = {" + ".join(arr[:-1])}')
+    else:
+        break
