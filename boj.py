f1b0adc74acc95c571ff43713a85499488fa2434
CWCTBOY <qkrdlswo98@gmail.com>
2022-04-20 17:12:20 +0900

    맞왜틀

--- boj.py
+++ boj.py
@@ -605,21 +605,30 @@
 #         print(int((A_origin*B_origin)/arr_ab[-1]))
 
 # 9506
-while True:
-    n = int(input())
-    n_origin = n
-    arr = ['1']
-    i = 1
-    if n != -1:
-        while n - i != 0:
-            i += 1
-            if n % i == 0:
-                arr.append(str(i))
-        arr_int = map(int, arr)
-        integrate = sum(arr_int) - n_origin
-        if integrate != n_origin:
-            print(f'{n_origin} is NOT perfect.')
-        else:
-            print(f'{n_origin} = {" + ".join(arr[:-1])}')
-    else:
-        break
+# while True:
+#     n = int(input())
+#     n_origin = n
+#     arr = ['1']
+#     i = 1
+#     if n != -1:
+#         while n - i != 0:
+#             i += 1
+#             if n % i == 0:
+#                 arr.append(str(i))
+#         arr_int = map(int, arr)
+#         integrate = sum(arr_int) - n_origin
+#         if integrate != n_origin:
+#             print(f'{n_origin} is NOT perfect.')
+#         else:
+#             print(f'{n_origin} = {" + ".join(arr[:-1])}')
+#     else:
+#         break
+
+# 1789
+S = int(input())
+arr = []
+i = 0
+while sum(arr) + i < S:
+    i += 1
+    arr.append(i)
+print(len(arr))
